5cf389bbd32b376c0827ca583eb8b88e6b72574f
ashyrovsasha18@icloud.com <ashyrovsasha18@icloud.com>
2022-11-18 00:00:45 +0500

    Внёс изменения и пошёл спать

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,30 +11,24 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
           child: Column(
             children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(0),
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: tdblack,
-                      size: 20,
-                    ),
-                    prefixIconConstraints:
-                        BoxConstraints(maxHeight: 28, minWidth: 25),
-                    border: InputBorder.none,
-                    hintText: 'Search',
-                    hintStyle: TextStyle(color: tdgray),
-                  ),
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w500),
+                        ))
+                  ],
                 ),
               ),
+              ToDoItem()
             ],
           )),
     );
@@ -59,4 +54,26 @@ class Home extends StatelessWidget {
               ))
         ]));
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(20)),
+    child: TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search,
+          color: tdblack,
+          size: 20,
+        ),
+        prefixIconConstraints: BoxConstraints(maxHeight: 28, minWidth: 25),
+        border: InputBorder.none,
+        hintText: 'Search',
+        hintStyle: TextStyle(color: tdgray),
+      ),
+    ),
+  );
 }
